7ffd483f71c2a487a9621b9cce47975c7ea516d5
Malcuthrad <antonioandresperezs@gmail.com>
2025-01-28 04:26:57 -0300

    update

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-let amigos = [];
-let nombre = document.getElementById("amigo");
 let lista = document.getElementById("resultado");
 
+let amigos = [];
+
 function agregarAmigo(){
-   if(isNaN(nombre.value)){
+    let nombre = document.getElementById("amigo");
+
+    if(/\d/.test(nombre.value)) {
+        alert("Ingresa un nombre valido");
+    } else if (amigos.includes(nombre.value)){
+        alert("Este nombre ya existe en la lista.")
+    } else{
     let li = document.createElement('li');
     let liText = document.createTextNode(nombre.value);
     li.appendChild(liText);
     lista.appendChild(li);
     amigos.push(nombre.value);
-    } else if (listaAmigos.includes(nombre.value)){
-        alert("Este Amigo ya fue agregado.")
-    } else{
-        alert("Ingresa un nombre valido");
+        
  }
  nombre.value = ""; 
 }
