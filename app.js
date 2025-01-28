779f39d3fc113b2b5caf19d77ccc3202a4230c2a
Malcuthrad <antonioandresperezs@gmail.com>
2025-01-28 03:54:05 -0300

    Test code

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let nombre = document.getElementById("amigo");
+let lista = document.getElementById("resultado");
+
+function agregarAmigo(){
+   if(isNaN(nombre.value)){
+    let li = document.createElement('li');
+    let liText = document.createTextNode(nombre.value);
+    li.appendChild(liText);
+    lista.appendChild(li);
+    amigos.push(nombre.value);
+    } else if (listaAmigos.includes(nombre.value)){
+        alert("Este Amigo ya fue agregado.")
+    } else{
+        alert("Ingresa un nombre valido");
+ }
+ nombre.value = ""; 
+}
