f425c58113401af4fc2a33efb00788d33cfa551e
Malcuthrad <antonioandresperezs@gmail.com>
2025-01-28 05:27:42 -0300

    Agregado comentarios al codigo.

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ let amigos = [];
 
 function agregarAmigo(){
     let nombre = document.getElementById("amigo");
-    if(/\d/.test(nombre.value)) {
+    if(/\d/.test(nombre.value)) {   // verifica que el nombre ingresado sea solo letras.
         alert("Ingresa un nombre valido");
-    } else if (amigos.includes(nombre.value)){
+    } else if (amigos.includes(nombre.value)){  // verifica que no se repitan nombres.
         alert("Este nombre ya existe en la lista.")
-    } else{
+    } else{ // ingresa un nombre a la lista
         let li = document.createElement('li');
         let liText = document.createTextNode(nombre.value);
         li.appendChild(liText);
         lista.appendChild(li);
         amigos.push(nombre.value);
     }
-nombre.value = ""; 
+nombre.value = ""; // deja vacio la caja de texto para el sgte nombre a ingresar.
+}
+
+function sortearAmigo(){
+    if (amigos.length > 0){  // verifica que el arreglo tenga elementos a sortear.
+    let winner = amigos[Math.floor(Math.random() * amigos.length)];// si hay nombres entonces sortea 1 al azar.
+    //alert(`El amigo secreto es ${winner}`);  // se puede dar el nombre elegido por alerta
+    lista.innerHTML = `El amigo secreto es ${winner}`; // o mostrarlo en el cuadro donde se ven los nombres ingresado.
+    amigos = []; // al sortear un nombre el arreglo queda vacio.
+    } else{
+        alert("No hay nombres en la lista para sortear.")  // si no hay nombres ingresado no se hace el sorteo.
+    }
 }
